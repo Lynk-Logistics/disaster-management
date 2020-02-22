a1aea1c0b255bfb01e552600ed717f930ae6218e
Gaurav <gauravrai568@gmail.com>
2020-02-22 14:49:02 +0530

    dashboard and custom message page dine

---
+++ app/src/main/java/com/example/myapplication/test.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.dashboard.*
+
+class test : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+//        ripple.startRippleAnimation()
+
+    }
+}
